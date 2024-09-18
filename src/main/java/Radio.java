3c58dd786d676ce6a097a48ebb9db2bff13b550a
arthur.salahutdinov <artes_93@mail.ru>
2024-09-18 23:59:05 +0300

    Initial commit with local project

--- src/main/java/Radio.java
+++ src/main/java/Radio.java
@@ -1,4 +1,60 @@
-package PACKAGE_NAME;
-
 public class Radio {
+    private int currentStation;
+    private int currentVolume;
+
+    // Получить текущую радиостанцию
+    public int getCurrentStation() {
+        return currentStation;
+    }
+
+    // Установить радиостанцию, если она в пределах 0-9
+    public void setCurrentStation(int station) {
+        if (station >= 0 && station <= 9) {
+            this.currentStation = station;
+        }
+    }
+
+    // Переключение на следующую станцию
+    public void nextStation() {
+        if (currentStation == 9) {
+            currentStation = 0;
+        } else {
+            currentStation++;
+        }
+    }
+
+    // Переключение на предыдущую станцию
+    public void prevStation() {
+        if (currentStation == 0) {
+            currentStation = 9;
+        } else {
+            currentStation--;
+        }
+    }
+
+    // Получить текущий уровень громкости
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    // Увеличить громкость на 1
+    public void increaseVolume() {
+        if (currentVolume < 100) {
+            currentVolume++;
+        }
+    }
+
+    // Уменьшить громкость на 1
+    public void decreaseVolume() {
+        if (currentVolume > 0) {
+            currentVolume--;
+        }
+    }
+
+    // Установить громкость, если она в пределах 0-100
+    public void setVolume(int volume) {
+        if (volume >= 0 && volume <= 100) {
+            this.currentVolume = volume;
+        }
+    }
 }
--- src/test/java/RadioTest.java
+++ src/test/java/RadioTest.java
@@ -1,4 +1,83 @@
-package PACKAGE_NAME;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class RadioTest {
+
+    @Test
+    public void shouldSetStationWithinRange() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(5);
+        assertEquals(5, radio.getCurrentStation());
+    }
+
+    @Test
+    public void shouldNotSetStationOutOfRange() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(10);
+        assertEquals(0, radio.getCurrentStation());
+    }
+
+    @Test
+    public void shouldSwitchToNextStation() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(8);
+        radio.nextStation();
+        assertEquals(9, radio.getCurrentStation());
+    }
+
+    @Test
+    public void shouldWrapAroundFrom9To0() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(9);
+        radio.nextStation();
+        assertEquals(0, radio.getCurrentStation());
+    }
+
+    @Test
+    public void shouldSwitchToPrevStation() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(1);
+        radio.prevStation();
+        assertEquals(0, radio.getCurrentStation());
+    }
+
+    @Test
+    public void shouldWrapAroundFrom0To9() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(0);
+        radio.prevStation();
+        assertEquals(9, radio.getCurrentStation());
+    }
+
+    @Test
+    public void shouldIncreaseVolume() {
+        Radio radio = new Radio();
+        radio.setVolume(50);
+        radio.increaseVolume();
+        assertEquals(51, radio.getCurrentVolume());
+    }
+
+    @Test
+    public void shouldNotIncreaseVolumeAboveMax() {
+        Radio radio = new Radio();
+        radio.setVolume(100);
+        radio.increaseVolume();
+        assertEquals(100, radio.getCurrentVolume());
+    }
+
+    @Test
+    public void shouldDecreaseVolume() {
+        Radio radio = new Radio();
+        radio.setVolume(50);
+        radio.decreaseVolume();
+        assertEquals(49, radio.getCurrentVolume());
+    }
+
+    @Test
+    public void shouldNotDecreaseVolumeBelowMin() {
+        Radio radio = new Radio();
+        radio.setVolume(0);
+        radio.decreaseVolume();
+        assertEquals(0, radio.getCurrentVolume());
+    }
 }
